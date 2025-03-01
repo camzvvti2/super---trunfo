363ce2efc9988574045d992efa520d5b606eef45
Alexandre Barros <103068697+alexandre21ssouza@users.noreply.github.com>
2025-03-01 21:38:15 +0000

    Criação dos atributos da carta número um

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,12 +8,33 @@
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, 
+    //número de pontos turísticos.
+
+    int carta_um = 1;
+    int carta_dois = 2;
+    char nome_do_pais[50];
+    char codigo_da_cidade[50];
+    float populacao;
+    float area_km;
+    float pib;
+    int pontos_turisticos;
+
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    printf("BEM-VINDO AO JOGO DE CARTAS SUPER TRUNFO!\n");
+    printf("Cadastre as Cartas:\n");
+    printf("Carta: %d\n", carta_um);
+    printf("Escreva o nome do País: ");
+    fgets(nome_do_pais,50,stdin);
+    printf("Nome do País: %s" , nome_do_pais);
+    printf("Escreva o código do País: ");
+
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
