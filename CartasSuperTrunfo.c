4c9e17447a934c041d30c621db59d95ca3b093ee
LukasKock <lukaskock@gmail.com>
2025-03-11 13:55:50 -0300

    1º teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,34 @@
 
 int main() {
 
+    char estado;
+    char codigo[3];
+    char cidade[20];
+    float area;
+    float pib;
+    int pontosTuristicos;
+ 
+    //coletando dados
+    printf("Digite a primeira letra de seu Estado:\n");
+    scanf(" %c", &estado);
+    printf("Digite o código de 01 a 04:\n");
+    scanf("%2s", &codigo);
+    printf("Digite o nome de sua cidade:\n");
+    scanf("%19s", &cidade);
+    printf("Digite a área de sua cidade em km²:\n");
+    scanf(" %f", &area);
+    printf("Digite o PIB de sua cidade:\n");
+    scanf(" %f", &pib);
+    printf("Digite o número de pontos turísticos de sua cidade:\n");
+    scanf(" %d", &pontosTuristicos);
+
+    //imprimindo dados
+    printf("\nSeu Estado é: %c\n", estado);
+    printf("Seu código é: %c%s\n", estado, codigo);
+    printf("Sua cidade é: %s\n", cidade);
+    printf("A área da sua cidade é: %.2f km²\n", area);
+    printf("O PIB de sua cidade é: %.2f\n", pib);
+    printf("O número de pontos turísticos são: %d\n\n", pontosTuristicos);
 
     return 0;
 }
