6775072b55e0e27120299884300b4480e6141ef4
Salatiel2909 <202721340+Salatiel2909@users.noreply.github.com>
2025-03-13 19:41:02 +0000

    adicionei as entradas da primeira carta

--- supertrunfo.c
+++ supertrunfo.c
@@ -2,8 +2,12 @@
 
 int main() {
     // Declaração das variáveis para armazenar os dados das cartas
+
+    /*o sistema básico do jogo Super Trunfo com o tema "Países". As cartas serão divididas por estados, cada um com quatro cidades. 
+     Imagine um país dividido em oito estados (A a H), e cada estado com quatro cidades (1 a 4).  A combinação forma o código da carta (ex: A01, B02).*/
+
     char estado1, estado2;
-    char codigo1[5], codigo2[5];
+    char codigo1, codigo2;
     char cidade1[100], cidade2[100];
     int populacao1, populacao2;
     float area1, area2;
@@ -14,11 +18,23 @@ int main() {
     printf("Estado: Uma letra de 'A' a 'H' (representando um dos oito estados) \n");
     scanf("%c", &estado1);
 
+    printf("Escolha a cidade desejada 1 a 4 (ex: São Paulo, Rio de Janeiro): \n");
+    scanf("%c", &cidade1);
 
+    /* printf("codigo 1 a 4 (ex:01,02): \n");
+    scanf("%c", &codigo1);
 
+    printf("Qual a populçao da Cidade escolhida? (ex: 1510023500): \n");
+    scanf("%d", &populacao1);
 
+    printf("Area da cidade escolhida: \n");
+    scanf("%f", &area1);
 
+    printf("PIB da cidade: \n");
+    scanf("%f", &pib1);
 
+    printf("Por ultimo, os Pontos Turistico da cidade: \n");
+    scanf("%d", &pontosTuristicos1);*/
 
 
 
@@ -33,12 +49,3 @@ int main() {
 
 }
 
-
-
-
-
-
-
-
-
-}
