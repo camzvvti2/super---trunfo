91713a5f9d5d4965958ec80ee009ba006aa7ae1e
VitorSansoni <vitor.sansoni.vs@gmail.com>
2025-03-17 03:41:14 +0000

    criei varios printf e scanf

--- cardum.c/cardum.c
+++ cardum.c/cardum.c
@@ -2,9 +2,30 @@
 
 int main () {
     printf ("Desafio Super Trunfo\n");
-    
 
+    char Estado [7]= 'Alagoas';
+    char Cidade[5] = 'Belem';
+    int População = 4.284;
+    float Área = 48.196;
+    float PIB = 17708.002;
+    int PontosTuristicos = 15;
 
+    printf ("Digite Estado\n");
+    scanf ("%s , &Estado %c");
 
+    printf ("Digite Cidade\n");
+    scanf ("%s , &Cidade %c");
 
-}
+    printf ("digite População\n");
+    scanf ("%d , &população");
+
+    printf ("Digite a Área\n");
+    scanf ("%d , &Área");
+
+    printf ("digite o PIB\n");
+    scanf ("%d , &PIB");
+
+    printf ("Digite PontosTuristicos\n");
+    scanf ("%i , &PontosTuristicos");
+
+} return0;
