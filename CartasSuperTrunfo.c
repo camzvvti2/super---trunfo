2e7248f95dd36cae9ae06a60a6e96196c8c5cdde
vbenedito <beneditoloura@gmail.com>
2025-03-18 09:54:34 +0000

    comparando valores das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,104 +1,83 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-
-    // declarando variaveis para a primeira cidade
-    char state1, cardCode1[5], cityName1[30];
-    int populationCity1, turistsPointCity1;
-    float kilometersAreaCity1, populationDensityCity1, PIBperCapitaCity1;
-    double PIBCity1;
-
-    printf("Informe os dados da primeira carta:\n");
+void inputData(char *state, char *cardCode, char *cityName, unsigned long int *populationCity, float *kilometersAreaCity, double *PIBCity, int *turistsPointCity) {
     printf("Digite a letra do estado (A-H): \n");
-    scanf(" %c", &state1);
+    scanf(" %c", state);
     getchar();
 
     printf("Digite o código da carta (ex: A01): \n");
-    scanf(" %4s", cardCode1);
+    scanf(" %4s", cardCode);
     getchar();
 
     printf("Digite o nome da cidade: \n");
-    fgets(cityName1, sizeof(cityName1), stdin);
-    cityName1[strcspn(cityName1, "\n")] = '\0'; // Remove o '\n'
+    fgets(cityName, sizeof(cityName), stdin);
+    cityName[strcspn(cityName, "\n")] = '\0'; // Remove o '\n'
 
     printf("Digite a população da cidade: \n");
-    scanf("%i", &populationCity1);
+    scanf("%lu", populationCity);
 
     printf("Digite a area (em km²) da cidade: \n");
-    scanf("%f", &kilometersAreaCity1);
+    scanf("%f", kilometersAreaCity);
 
     printf("Digite o PIB da cidade: \n");
-    scanf("%lf", &PIBCity1);
+    scanf("%lf", PIBCity);
 
     printf("Digite número de pontos turisticos da cidade: \n");
-    scanf("%d", &turistsPointCity1);
+    scanf("%d", turistsPointCity);
+}
 
+float calculatePopulationDensity(unsigned long int populationCity, float kilometersAreaCity) {
+    return (float)populationCity / kilometersAreaCity;
+}
+
+float calculatePIBPerCapita(double PIBCity, unsigned long int populationCity) {
+    return PIBCity / (float)populationCity;
+}
+
+float calculateSuperPower(unsigned long int populationCity, float kilometersAreaCity, int turistsPointCity, double PIBCity, float populationDensityCity) {
+    return (float)populationCity + kilometersAreaCity + (float)PIBCity + turistsPointCity + PIBCity + (1 / populationDensityCity);
+}
+
+int main(){
+    // declarando variaveis para a primeira cidade
+    char state1, cardCode1[5], cityName1[30];
+    unsigned long int populationCity1;
+    int turistsPointCity1;
+    float kilometersAreaCity1, populationDensityCity1, PIBperCapitaCity1, superPowerCity1;
+    double PIBCity1;
+
+    printf("Informe os dados da primeira carta:\n");
+    inputData(&state1, cardCode1, cityName1, &populationCity1, &kilometersAreaCity1, &PIBCity1, &turistsPointCity1);
     printf("Todos os dados da primeira carta foram adicionados com sucesso. Vamos para os dados da segunda carta\n");
 
     // declarando variáveis para a segunda cidade
     char state2, cardCode2[5], cityName2[30];
-    int populationCity2, turistsPointCity2;
+    unsigned long int populationCity2;
+    int turistsPointCity2;
     float kilometersAreaCity2, populationDensityCity2, PIBperCapitaCity2;
     double PIBCity2;
 
     printf("Informe os dados da segunda carta:\n");
-    printf("Digite a letra do estado (A-H): \n");
-    scanf(" %2s", &state2);
-    getchar();
+    inputData(&state2, cardCode2, cityName2, &populationCity2, &kilometersAreaCity2, &PIBCity2, &turistsPointCity2);
 
-    printf("Digite o código da carta (ex: A01): \n");
-    scanf(" %4s", cardCode2);
-    getchar();
-
-    printf("Digite o nome da cidade: \n");
-    fgets(cityName2, sizeof(cityName2), stdin);
-    cityName2[strcspn(cityName2, "\n")] = '\0'; // Remove o '\n'
-
-    printf("Digite a população da cidade: \n");
-    scanf("%i", &populationCity2);
-
-    printf("Digite a area (em km²) da cidade: \n");
-    scanf("%f", &kilometersAreaCity2);
-
-    printf("Digite o PIB da cidade: \n");
-    scanf("%lf", &PIBCity2);
-
-    printf("Digite número de pontos turisticos da cidade: \n");
-    scanf("%d", &turistsPointCity2);
+    float densityCity1 = calculatePopulationDensity(populationCity1, kilometersAreaCity1);
+    float densityCity2 = calculatePopulationDensity(populationCity2, kilometersAreaCity2);
+    float calculatePIBPerCapitaCity1 = calculatePIBPerCapita(PIBCity1, populationCity1);
+    float calculatePIBPerCapitaCity2 = calculatePIBPerCapita(PIBCity2, populationCity2);
+    float calculateSuperPowerCity1 = calculateSuperPower(populationCity1, kilometersAreaCity1, turistsPointCity1, PIBCity1, densityCity1);
+    float calculateSuperPowerCity2 = calculateSuperPower(populationCity2, kilometersAreaCity2, turistsPointCity2, PIBCity2, densityCity2);
 
     
-    // retornando as informações inseridas pelo usuário sobre a primeira cidade
-    printf("Carta 1: \n");
-    printf("Estado: %c\n", state1);
-    printf("Código: %s\n", cardCode1);
-    printf("Nome da Cidade: %s\n", cityName1);
-    printf("População: %d\n", populationCity1);
-    printf("Área: %.2f km²\n", kilometersAreaCity1);
-    printf("PIB: %.2lf bilhões de reais\n", PIBCity1 / 1e9);
-    printf("Número de Pontos Turísticos: %d\n", turistsPointCity1);
+    // retornando a comparação entre duas cartas
+    printf("Comparação de Cartas:\n");
+    printf("População: Carta %d venceu (%d)\n", (populationCity1 > populationCity2) ? 1 : 2, populationCity1 > populationCity2);
+    printf("Área: Carta %d venceu (%d)\n", (kilometersAreaCity1 > kilometersAreaCity2) ? 1 : 2, kilometersAreaCity1 > kilometersAreaCity2);
+    printf("PIB: Carta %d venceu (%d)\n", (PIBCity1 > PIBCity2) ? 1 : 2, PIBCity1 > PIBCity2);
+    printf("Pontos Turisticos: Carta %d venceu (%d)\n", (turistsPointCity1 > turistsPointCity2) ? 1 : 2, turistsPointCity1 > turistsPointCity2);
+    printf("Densidade Populacional: Carta %d venceu (%d)\n", (densityCity1 < densityCity2) ? 1 : 2, densityCity1 < densityCity2);
+    printf("PIB per Capita: Carta %d venceu (%d)\n", (calculatePIBPerCapitaCity1 > calculatePIBPerCapitaCity2) ? 1 : 2, calculatePIBPerCapitaCity1 > calculatePIBPerCapitaCity2);
+    printf("Super Poder: Carta %d venceu (%d)\n", (calculateSuperPowerCity1 > calculateSuperPowerCity2) ? 1 : 2, calculateSuperPowerCity1 > calculateSuperPowerCity2);
 
-    populationDensityCity1 = (float)populationCity1 / kilometersAreaCity1;
-    printf("Densidade Populacional: %.2f hab/km²\n", populationDensityCity1);
-
-    PIBperCapitaCity1 = PIBCity1 / (float)populationCity1;
-    printf("PIB per Capita: %.2f reais\n", PIBperCapitaCity1);
-
-    // retornando as informações inseridas pelo usuário sobre a segunda cidade
-    printf("Carta 2: \n");
-    printf("Estado: %c\n", state2);
-    printf("Código: %s\n", cardCode2);
-    printf("Nome da Cidade: %s\n", cityName2);
-    printf("População: %d\n", populationCity2);
-    printf("Área: %.2f km²\n", kilometersAreaCity2);
-    printf("PIB: %.2lf bilhões de reais\n", PIBCity2 / 1e9);
-    printf("Número de Pontos Turísticos: %d\n", turistsPointCity2);
-
-    populationDensityCity2 = (float)populationCity2 / kilometersAreaCity2;
-    printf("Densidade Populacional: %.2f hab/km²\n", populationDensityCity2);
-
-    PIBperCapitaCity2 = PIBCity2 / (float)populationCity2;
-    printf("PIB per Capita: %.2f reais\n", PIBperCapitaCity2);
-    
     return 0;
 }
