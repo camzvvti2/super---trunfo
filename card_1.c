c8afee7518156b5242efe790dd97b20f3fb5f6fd
VitorSansoni <vitor.sansoni.vs@gmail.com>
2025-03-18 18:42:53 +0000

    cosntruindo a carta super trunfo

---
+++ card_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    int População = 120000;
+    
+    printf ("Digite a População:");
+    scanf ("%d" , &População );
+    printf ("A População é: %d" , População);
+
+
+
+
+
+}
