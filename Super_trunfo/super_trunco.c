904ed667475ae188d8af709f0d829da7719c231f
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-20 14:34:49 +0000

    declarei dados criei printscan para mostrar dados inseridos

---
+++ Super_trunfo/super_trunco.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("CARTA 01\n");
+
+    char CodigoCarta[03] = "A01";
+    char Estado[06] = "BAHIA";
+    char cidade[10] = "SALVADOR";
+    int populacao = 2568928;
+    float areakm2 = 693.8;
+    int NumeroPontosTuristicos = 20;
+
+    printf("O Codigo da Carta é: %s\n", "A01");
+    printf("O Estado é a: %s\n", "Bahia");
+    printf("A Cidade se Chama: %s\n", "Salvador");
+    printf("A População de Salvador é: %d\n", 2568928);
+    printf("A cidade tem uma Área em KM² de: %f\n", 693.8);
+    printf("O número de pontos turisticos são: %i\n", 20);
+
+    
+
+
+}
