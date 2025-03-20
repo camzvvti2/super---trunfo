6aebe5d286cab033fec6b9b8fd932c717aa94a00
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-20 14:36:01 +0000

    alteraçaõ de declaração de dados - ana paula

--- Super_trunfo/super_trunco.c
+++ Super_trunfo/super_trunco.c
@@ -4,6 +4,8 @@ int main(){
 
     printf("CARTA 01\n");
 
+    //Acima criado printf para identicar primeira carta do Super_Trunfo;
+
     char CodigoCarta[03] = "A01";
     char Estado[06] = "BAHIA";
     char cidade[10] = "SALVADOR";
@@ -11,12 +13,16 @@ int main(){
     float areakm2 = 693.8;
     int NumeroPontosTuristicos = 20;
 
+    //Acima declarei variveis com os nomes adequados;
+
     printf("O Codigo da Carta é: %s\n", "A01");
     printf("O Estado é a: %s\n", "Bahia");
     printf("A Cidade se Chama: %s\n", "Salvador");
     printf("A População de Salvador é: %d\n", 2568928);
     printf("A cidade tem uma Área em KM² de: %f\n", 693.8);
     printf("O número de pontos turisticos são: %i\n", 20);
+  
+    //Criei printscan para mostrar dados inseridos;
 
     
 
