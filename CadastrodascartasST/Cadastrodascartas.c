bfbdcfe17f98240da223f86619dfe3721200a6d5
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-20 00:56:02 +0000

    declarei variaveis

---
+++ CadastrodascartasST/Cadastrodascartas.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Cadastro das Cartas Super Trunfo\n");
+
+        int Codigodecarta;
+        char Cidades[30];
+        int População;
+        float Área;
+        float Pib;
+        int NúmeroPontoTurisitico;
+
+        int Codigodecarta;
+        char Cidades[30];
+        int População;
+        float Área;
+        float Pib;
+        int NúmeroPontoTurisitico;
+
+    
+}
