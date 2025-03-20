d83329275db7a61cfd58b744eab2ccc7e5c29f5f
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-20 15:36:24 +0000

    PAULA PRINT SCAN

--- Super_trunfo/super_trunfo.c
+++ Super_trunfo/super_trunfo.c
@@ -6,7 +6,7 @@ int main(){
 
     //Acima criado printf para identicar primeira carta do Super_Trunfo;
 
-    char CodigoCarta[03] = "A01";
+    char CodigoCarta[3] = 'A1';
     char Estado[06] = "BAHIA";
     char cidade[10] = "SALVADOR";
     int populacao = 2568928;
@@ -15,16 +15,29 @@ int main(){
 
     //Acima declarei variveis com os nomes adequados;
 
-    printf("O Codigo da Carta é: %s\n", "A01");
+    printf("O Codigo da Carta é: %s\n", 'A1');
     printf("O Estado é a: %s\n", "Bahia");
     printf("A Cidade se Chama: %s\n", "Salvador");
     printf("A População de Salvador é: %d\n", 2568928);
     printf("A cidade tem uma Área em KM² de: %f\n", 693.8);
     printf("O número de pontos turisticos são: %i\n", 20);
   
-    //Criei printscan para mostrar dados inseridos;
+    //Acima Criei printscan para mostrar dados inseridos \n;
 
-    
+    printf("Digite o codigo da carta: ");
+    scanf("%s", '&A1');
+
+    printf("Digite o Estado e a cidade: \n");
+    scanf("%s", "&Bahia", "&Salvador");
+
+    printf("Digite a população da cidade: \n");
+    scanf("%d", 25689298);
+
+    printf("Digite a área da cidade em km²: \n");
+    scanf("%f\n", 693.8);
+
+    printf("Digite o número de pontos turisticos da Cidade: \n");
+    scanf("%i", 20);
 
 
 }
