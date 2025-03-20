8773ec4cc3a2cf1e9491761d6ea708e4f6c53f10
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-20 01:09:08 +0000

    CRIEI DECLARAÇÃO DE VARIAVEIS

--- CadastrodascartasST/Cadastrodascartas.c
+++ CadastrodascartasST/Cadastrodascartas.c
@@ -11,12 +11,27 @@ int main(){
         float Pib;
         int NúmeroPontoTurisitico;
 
-        int Codigodecarta;
+        char Codigodecarta[03];
         char Cidades[30];
         int População;
         float Área;
         float Pib;
         int NúmeroPontoTurisitico;
+
+        char Codigodecarta[03] = "A01";
+        char Cidades[40] = ("SÃO PAULO, BAHIA, tOCANTINS, PIAUI");
+        int População = 66;
+        float Área = (1.345);
+        float Pib = (3.532);
+        int NúmeroPontoTurisitico = 57;
+
+        int Codigodecarta[03] = "B01";
+        char Cidades[45] = ("PERNAMBUCO, MARANHÃO, RIO DE JANERO, CEARÁ");
+        int População = (43);
+        float Área = (618.000);
+        float Pib = (3.597);
+        int NúmeroPontoTurisitico = (79);
+
 
     
 }
