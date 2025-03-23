090b54f09b39525f71d7b8bb4f6c8933498b9278
Antônio Felipe B. Archer <61703826+archeraf@users.noreply.github.com>
2025-03-23 16:09:38 +0000

    Desafio final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,18 +2,28 @@
 
 //Declaração de variáveis globais
 char estado, estado2, codigo[4], codigo2[4], nomeCidade[20], nomeCidade2[20];
-int populacao, populacao2, qtdPontosTur, qtdPontosTur2;
-float area, area2, pib, pib2,densidadePop,densidadePop2, pibCapita, pibCapita2;
+int qtdPontosTur, qtdPontosTur2,vencedorPop, vencedorArea,vencedorPib,vencedorPontosTur,vencedorDensPop,vencedorPibCapita,vencedorSuperPod;
+float area, area2, pib, pib2,densidadePop,densidadePop2, pibCapita, pibCapita2, superPod, superPod2;
+unsigned long int populacao, populacao2;
 
 
+
+//Método responsável por calcular e retornar o PIB per capita
 float calcularPibPerCapita(int populacao, float pib){
     return pib/populacao;
 }
 
+//Método responsável por calcular e retornar a densidade populacional
 float calcularDensidadePop(int populacao, float area){
     return (float)populacao/area;
 }
 
+//Método responsável por calcular e retornar o super poder
+float calcularSuperPod(int populacao, int qtdPontosTur, float area, float pib, float densidadePop, float pibCapita){
+    return (float)populacao + (float)qtdPontosTur + area + pib + densidadePop + pibCapita;
+}
+
+//Métodos responsáveis pela coleta de dados das cartas
 void ColetarDados(){
     printf("Digite o Estado:\n");
     scanf(" %c", &estado);
@@ -39,6 +49,7 @@ void ColetarDados(){
 
     pibCapita = (float)calcularPibPerCapita(populacao,pib);
     densidadePop = calcularDensidadePop(populacao,area);
+    superPod = calcularSuperPod(populacao, qtdPontosTur, area, pib, densidadePop, pibCapita);
 }
 
 void ColetarDados2(){
@@ -66,8 +77,9 @@ void ColetarDados2(){
 
     pibCapita2 = calcularPibPerCapita(populacao2,pib2);
     densidadePop2 = calcularDensidadePop(populacao2,area2);
-
+    superPod2 = calcularSuperPod(populacao2, qtdPontosTur2, area2, pib2, densidadePop2, pibCapita2);
 }
+
 //Método responsável pela exibição dos dados
 void ExibirDados(){
     printf("Carta 1:\n");
@@ -80,6 +92,7 @@ void ExibirDados(){
     printf("Area da cidade: %.2f\n", area);
     printf("Densidade populacional: %.2f\n", densidadePop);
     printf("PIB per Capita: %.2f\n", pibCapita);
+    printf("Super poder: %.2f\n", superPod);
 
 
     //Exibindo dados da segunda carta
@@ -93,15 +106,39 @@ void ExibirDados(){
     printf("Area da cidade: %.2f \n", area2);
     printf("Densidade populacional: %.2f\n", densidadePop2);
     printf("PIB per Capita: %.2f\n", pibCapita2);
+    printf("Super poder: %.2f\n", superPod2);
 
 }
 
+//Método responsável pela comparação dos atributos das duas cartas
+void CompararCartas(){
+    vencedorPop = populacao > populacao2;
+    vencedorPib = pib > pib2;
+    vencedorPibCapita = pibCapita > pibCapita2;
+    vencedorArea = area > area2;
+    vencedorDensPop = densidadePop < densidadePop2;
+    vencedorPontosTur = qtdPontosTur > qtdPontosTur2;
+    vencedorSuperPod = superPod > superPod2;
+}
+
+void ExibirResultadoComparacoes(){
+    printf("Comparação de Cartas:\n");
+    printf("População da carta 1 ganhou da população da carta 2? R: %i\n", vencedorPop);
+    printf("Pib da carta 1 ganhou do Pib da carta 2? R: %i\n", vencedorPib);
+    printf("Pib per capita da carta 1 ganhou do Pib per capita da carta 2? R: %i\n", vencedorPibCapita);
+    printf("Area da carta 1 ganhou da Area da carta 2? R: %i\n", vencedorArea);
+    printf("Densidade populacional da carta 1 ganhou da densidade populacional da carta 2? R: %i\n", vencedorPop);
+    printf("Quantidade de pontos turísiticos da carta 1 ganhou da quantidade de pontos turísiticos da carta 2? R: %i\n", vencedorPop);
+    printf("Super poder da carta 1 ganhou do super poder da carta 2? R: %i\n", vencedorPop);
+}
 
 //Método main com a chamada para os demais métodos
 int main() {
    ColetarDados();
    ColetarDados2();
+   CompararCartas();
    ExibirDados();
+   ExibirResultadoComparacoes();
 
     return 0;
 }
