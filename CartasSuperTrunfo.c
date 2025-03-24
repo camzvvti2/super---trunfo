3bc06ae20f8e2fd96dfd95655bb90534e6238903
Rodolfo <202503832706@alunos.estacio.br>
2025-03-23 23:43:55 -0300

    Update CartasSuperTrunfo.c
    adicionando mais informações do passo a passo da criação do programa.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
+    //Declarando as variaveis e seus nomes char,int,float
+    
     char estado;
     char codigo[4];  // 3 caracteres + '\0' para segurança
     char nome[20];   // Nome aumentado para permitir nomes compostos
@@ -8,6 +10,8 @@ int main() {
     float area, Pib;
 
     // Cadastro das duas cartas, carta 1 e carta 2
+    //imprimindo a solicitação de  informações de talhadas.
+
     for (int i = 1; i <= 2; i++) {
         printf("\n=== Cadastro da Carta %d ===\n", i);
 
@@ -32,7 +36,7 @@ int main() {
         printf("PIB da cidade (em milhões): ");
         scanf("%f", &Pib);
 
-        // Exibição dos dados cadastrados
+        // Exibição dos dados cadastrados das cartas 1 e 2 respectivamente
         printf("\n=== Dados da Carta %d ===\n", i);
         printf("Estado: %c | Código: %s | Nome: %s\n", estado, codigo, nome);
         printf("População: %d | Pontos turísticos: %d\n", populacao, N_P_turistico);
