8705cb7823e5a8a89f26b7b8a939936833d496d0
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-25 23:57:46 +0000

    atualiza configuração do VSCode e corrige declaração de variáveis no código do Super Trunfo

--- Super_trunfo/supertunfoteste.c
+++ Super_trunfo/supertunfoteste.c
@@ -4,22 +4,25 @@ int main(){
 
     printf("CARTA 01\n");
 
-    //Acima criado printf para identicar primeira carta do Super_Trunfo;
-
-    char CodigoCarta[03] = "A01";
+    int codigodacarta = 01;
     char Estado[06] = "BAHIA";
     char cidade[10] = "SALVADOR";
     int populacao = 2568928;
     float areakm2 = 693.8;
     int NumeroPontosTuristicos = 20;
 
-    //Acima declarei variveis com os nomes adequados;
-
-    printf("O Codigo da Carta é: %s\n", 'A1');
+    printf("O codigo da carta e: %d\n", 01);
     printf("O Estado é a: %s\n", "Bahia");
     printf("A Cidade se Chama: %s\n", "Salvador");
     printf("A População de Salvador é: %d\n", 2568928);
     printf("A cidade tem uma Área em KM² de: %f\n", 693.8);
     printf("O número de pontos turisticos são: %i\n", 20);
-  
-    //Acima Criei printscan para mostrar dados inseridos \n;
+
+
+    printf("Digite o numero docodigo da carta: \n");
+    
+    printf("Digite o estado: \n");
+
+
+
+}
