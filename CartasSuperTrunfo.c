b3ebed7039b38fc29b706e9fbb63d6934a460e27
Cyboxz <mathmonteiro@outlook.com>
2025-03-25 22:18:50 +0000

    Mudanças para melhor leitura

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,13 @@
 
 int main() {
 
+
+     //Declaração das variaveis da primeira carta
+
     int pontosturisticos, populacao;
     float area, pib, densidadepopulacional, PIBpercapita;
     char nomedacidade[50], codigodacidade[50], estado[50];
+    float SuperPoder1;
 
     printf("Digite o Código da cidade: \n");
     scanf("%s", &codigodacidade);
@@ -38,35 +42,59 @@ int main() {
     printf("Pontos turísticos da cidade: %d \n", pontosturisticos);
     printf("A densidade populacional é: %fhab/km² \n", densidadepopulacional);
     printf("O PIB per capita é: %freais \n", PIBpercapita);
+
+    printf(" O valor do super trunfo da carta A (%f) é maior que o valor total da carta de superpoder B (%f)? %d\n",
+    SuperPoder1 = codigodacidade + nomedacidade + populacao + area + pib + pontosturisticos + densidadepopulacional + PIBpercapita;
+                        
     
 
+    //Declaração das variaveis da segunda carta
+    
+    int pontosturisticos2, populacao2;
+    float area2, pib2, densidadepopulacional2, PIBpercapita2;
+    char nomedacidade2[50], codigodacidade2[50], estado2[50];
+    float SuperPoder2
+    
+    printf("Carta 2: \n");
+    
     printf("Digite o Código da cidade: \n");
-    scanf("%s", &codigodacidade);
+    scanf("%s", &codigodacidade2);
 
     printf("Digite o estado da cidade: \n");
-    scanf("%s", &estado);
+    scanf("%s", &estado2);
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &nomedacidade);
+    scanf("%s", &nomedacidade2);
 
     printf("Digite a população da cidade: \n");
-    scanf("%d", &populacao);
+    scanf("%d", &populacao2);
 
     printf("Digite a área da cidade: \n");
-    scanf("%f", &area);
+    scanf("%f", &area2);
 
     printf("Digite o PIB da cidade: \n");
-    scanf("%f", &pib);
+    scanf("%f", &pib2);
 
     printf("Digite quantos pontos turísticos há na cidade: \n");
-    scanf("%d", &pontosturisticos);
+    scanf("%d", &pontosturisticos2);
 
-    printf("Código da cidade: %s \n", codigodacidade);
-    printf("Nome da cidade: %s \n", nomedacidade);
-    printf("População da cidade: %d \n", populacao);
-    printf("Área da cidade: %f \n", area);
-    printf("PIB da cidade: %f\n", pib);
-    printf("Pontos turísticos da cidade: %d\n", pontosturisticos);
+    densidadepopulacional2 = (populacao2 / area2);
+    PIBpercapita2 = (pib2 / populacao2);
+
+    SuperPoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + PIBpercapita2 + densidadepopulacional2;
+
+    printf("Código da cidade: %s \n", codigodacidade2);
+    printf("Nome da cidade: %s \n", nomedacidade2);
+    printf("População da cidade: %.d \n", populacao2);
+    printf("Área da cidade: %.2fkm2 \n", area2);
+    printf("PIB da cidade: R$ %.4f \n", pib2);
+    printf("Pontos turísticos da cidade: %d \n", pontosturisticos2);
+    printf("A densidade populacional é: %fhab/km² \n", densidadepopulacional2);
+    printf("O PIB per capita é: %freais \n", PIBpercapita2);
+
+
+    printf("Digite o primeiro valor que gostaria de comparar"\n);
+
 
 
     return 0;
