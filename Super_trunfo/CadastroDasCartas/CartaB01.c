82fce19aa24f12039237607b1d7c73dda0767c23
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-26 00:56:44 +0000

    innicialização das varaveis e solicitação de dados

--- Super_trunfo/CadastroDasCartas/CartaB01.c
+++ Super_trunfo/CadastroDasCartas/CartaB01.c
@@ -2,4 +2,30 @@
 
 int main() {
     printf("Cadastro da Carta B01\n");
+    // Acima printf para definir qual carta está sendo cadastrada
+    // Abaixo definição de variáveis para cada atributo da carta
+    char Estado[1] = "B";
+    char CodigodaCarta[03] = "B01";
+    char Cidade[20] = "Fortaleza";
+    int Populacao = 2428678;
+    float Area = 312.3;
+    float PIB = 74.4;
+    int PontosTuristicos = 17;
+
+    // Abaixo solicitação de entrada de dados para cada atributo da carta
+    printf("Digite o Estado: ");
+    scanf("%s", Estado);
+    printf("Digite o Codigo da Carta: ");
+    scanf("%s", CodigodaCarta);
+    printf("Digite o Nome da cidade: ");
+    scanf("%s", Cidade);
+    printf("Digite a Populacao: ");
+    scanf("%d", &Populacao);
+    printf("Digite a Area: ");
+    scanf("%f", &Area);
+    printf("Digite o PIB: ");
+    scanf("%f", &PIB);
+    printf("Digite o Numero de Pontos Turisticos:  \n");
+    scanf("%d", &PontosTuristicos);
+    
 }
