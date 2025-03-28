4b0cd8bf046977e309b229ac6db86496508bdb34
Filipe <162770790+filoroch@users.noreply.github.com>
2025-03-28 13:18:58 +0000

    feat(CartasSuperTrunfo): adicionando struct e criando variaveis com base no modelo padrão das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,17 +6,28 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+typedef struct{
+    int number_card;
+    char state;
+    char cod_card[5];
+    char city[50];
+    int population, tourist_attractions;
+    float area, PIB;
+} Card;
+
+
+
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+ 
     // Cadastro das Cartas:
+    Card card1, card2;
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    // Receber os valores de card 1
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
