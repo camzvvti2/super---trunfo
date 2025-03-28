82e97b00c3af773955a78c1b2f67656c95575954
Filipe <162770790+filoroch@users.noreply.github.com>
2025-03-28 13:20:46 +0000

    feat(CartasSuperTrunfo): definição das funções receive_card para receber os dados por printf e scanf/fgets e print_card para imprimir os dados salvos no struct

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,7 +15,50 @@ typedef struct{
     float area, PIB;
 } Card;
 
+void receive_card(Card *c, int number){
+    c->number_card = number;
 
+    printf("\n----------------------------------\n");
+    printf("Digite uma letra de A a H (representando os estados)\n");
+    scanf(" %c", &c->state);
+
+    printf("Digite o código do carta. O codigo é composto da letra do estado (digitada anteriormente)\n");
+    scanf("%s", c->cod_card);
+
+    // Limpa o buffer de entrada
+    scanf("%*[^\n]");
+    scanf("%*c");
+    
+    printf("Digite o nome da cidade\n");
+    fgets(c->city, 50, stdin);
+    c->city[strcspn(c->city, "\n")] = 0;
+
+    printf("Digite o numero de pessoas que residem na cidade (população)\n");
+    scanf("%d", &c->population);
+
+    printf("Digite a área da cidade\n");
+    scanf("%f", &c->area);
+
+    printf("Digite o PIB da cidade\n");
+    scanf("%f", &c->PIB);
+
+    printf("Digite o numero de pontos turisticos da cidade\n");
+    scanf("%d", &c->tourist_attractions);
+}
+
+void print_card(Card *c){
+    printf("\n");
+    printf("Carta %d:\n", c->number_card);
+    
+    printf("Estado: %c\n", c->state);
+    // testar o concatenar de strings do estado + a entrada do usuario para o codigo da carta
+    printf("Código da carta: %s\n", c->cod_card); 
+    printf("Nome da cidade: %s\n", c->city);
+    printf("População: %d\n", c->population);
+    printf("Área: %.2f km²\n", c->area);
+    printf("PIB: %.2f bilhoes de reais\n", c->PIB);
+    printf("Número de Pontos Turísticos: %d\n", c->tourist_attractions);
+}
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -24,8 +67,8 @@ int main() {
     // Cadastro das Cartas:
     Card card1, card2;
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc
+    
     // Receber os valores de card 1
     
 
