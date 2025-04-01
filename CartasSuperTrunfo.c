52dd69db22ae9bcb07a42901fe8a49a3575b4bcf
vbenedito <beneditoloura@gmail.com>
2025-04-01 08:01:37 -0300

    adicionando switch case

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,86 +1,185 @@
 #include <stdio.h>
 #include <string.h>
 
-void inputData(char *state, char *cardCode, char *cityName, int *populationCity, float *kilometersAreaCity, double *PIBCity, int *turistsPointCity) {
+void inputData(char *state, char *cardCode, char *countryName, int countryNameSize, int *populationCountry, float *kilometersAreaCountry, double *PIBCountry, int *turistsPointCountry)
+{
     printf("Digite a letra do estado (A-H): \n");
     scanf(" %c", state);
-    getchar();
+    getchar(); // Remove o '\n' do buffer
 
     printf("Digite o código da carta (ex: A01): \n");
     scanf(" %4s", cardCode);
-    getchar();
+    getchar(); // Remove o '\n' do buffer
 
     printf("Digite o nome da cidade: \n");
-    fgets(cityName, sizeof(cityName), stdin);
-    cityName[strcspn(cityName, "\n")] = '\0'; // Remove o '\n'
+    fgets(countryName, countryNameSize, stdin);
+    countryName[strcspn(countryName, "\n")] = '\0'; // Remove o '\n' da string
 
     printf("Digite a população da cidade: \n");
-    scanf("%d", populationCity);
+    scanf("%d", populationCountry);
 
-    printf("Digite a area (em km²) da cidade: \n");
-    scanf("%f", kilometersAreaCity);
+    printf("Digite a área (em km²) da cidade: \n");
+    scanf("%f", kilometersAreaCountry);
 
     printf("Digite o PIB da cidade: \n");
-    scanf("%lf", PIBCity);
+    scanf("%lf", PIBCountry);
 
-    printf("Digite número de pontos turisticos da cidade: \n");
-    scanf("%d", turistsPointCity);
+    printf("Digite o número de pontos turísticos da cidade: \n");
+    scanf("%d", turistsPointCountry);
 }
 
-float calculatePopulationDensity(unsigned long int populationCity, float kilometersAreaCity) {
-    return (float)populationCity / kilometersAreaCity;
+float calculatePopulationDensity(unsigned long int populationCountry, float kilometersAreaCountry)
+{
+    return (float)populationCountry / kilometersAreaCountry;
 }
 
-float calculatePIBPerCapita(double PIBCity, unsigned long int populationCity) {
-    return PIBCity / (float)populationCity;
+float calculatePIBPerCapita(double PIBCountry, unsigned long int populationCountry)
+{
+    return PIBCountry / (float)populationCountry;
 }
 
-float calculateSuperPower(unsigned long int populationCity, float kilometersAreaCity, int turistsPointCity, double PIBCity, float populationDensityCity) {
-    return (float)populationCity + kilometersAreaCity + (float)PIBCity + turistsPointCity + PIBCity + (1 / populationDensityCity);
+float calculateSuperPower(unsigned long int populationCountry, float kilometersAreaCountry, int turistsPointCountry, double PIBCountry, float populationDensityCountry)
+{
+    return (float)populationCountry + kilometersAreaCountry + (float)PIBCountry + turistsPointCountry + PIBCountry + (1 / populationDensityCountry);
 }
 
+int main()
+{
 
-void compairCityProperty(int *cityProperty1, int *cityProperty2, char *cityName1, char *cityName2) {
-    if (cityProperty1 > cityProperty2) {
-        printf("Resultado: Carta 1 (%s) venceu!", cityName1);
-    } else {
-        printf("Resultado: Carta 2 (%s) venceu!", cityName2);
-    }
-}
-
-int main(){
-    // declarando variaveis para a primeira cidade
-    char state1, cardCode1[5], cityName1[30];
-    int turistsPointCity1, populationCity1;
-    float kilometersAreaCity1, populationDensityCity1, PIBperCapitaCity1, superPowerCity1;
-    double PIBCity1;
+    // declarando variaveis para o primeiro pais
+    char state1, cardCode1[5], countryName1[30];
+    int turistsPointCountry1, populationCountry1;
+    float kilometersAreaCountry1, populationDensityCountry1, PIBperCapitaCountry1, superPowerCountry1;
+    double PIBCountry1;
 
     printf("Informe os dados da primeira carta:\n");
-    inputData(&state1, cardCode1, cityName1, &populationCity1, &kilometersAreaCity1, &PIBCity1, &turistsPointCity1);
+    inputData(&state1, cardCode1, countryName1, sizeof(countryName1), &populationCountry1, &kilometersAreaCountry1, &PIBCountry1, &turistsPointCountry1);
     printf("Todos os dados da primeira carta foram adicionados com sucesso. Vamos para os dados da segunda carta\n");
 
-    // declarando variáveis para a segunda cidade
-    char state2, cardCode2[5], cityName2[30];
-    int turistsPointCity2, populationCity2;
-    float kilometersAreaCity2, populationDensityCity2, PIBperCapitaCity2;
-    double PIBCity2;
+    // declarando variáveis para o segundo pais
+    char state2, cardCode2[5], countryName2[30];
+    int turistsPointCountry2, populationCountry2;
+    float kilometersAreaCountry2, populationDensityCountry2, PIBperCapitaCountry2;
+    double PIBCountry2;
 
     printf("Informe os dados da segunda carta:\n");
-    inputData(&state2, cardCode2, cityName2, &populationCity2, &kilometersAreaCity2, &PIBCity2, &turistsPointCity2);
+    inputData(&state2, cardCode2, countryName2, sizeof(countryName2), &populationCountry2, &kilometersAreaCountry2, &PIBCountry2, &turistsPointCountry2);
 
-    float densityCity1 = calculatePopulationDensity(populationCity1, kilometersAreaCity1);
-    float densityCity2 = calculatePopulationDensity(populationCity2, kilometersAreaCity2);
-    float calculatePIBPerCapitaCity1 = calculatePIBPerCapita(PIBCity1, populationCity1);
-    float calculatePIBPerCapitaCity2 = calculatePIBPerCapita(PIBCity2, populationCity2);
-    float calculateSuperPowerCity1 = calculateSuperPower(populationCity1, kilometersAreaCity1, turistsPointCity1, PIBCity1, densityCity1);
-    float calculateSuperPowerCity2 = calculateSuperPower(populationCity2, kilometersAreaCity2, turistsPointCity2, PIBCity2, densityCity2);
+    float densityCountry1 = calculatePopulationDensity(populationCountry1, kilometersAreaCountry1);
+    float densityCountry2 = calculatePopulationDensity(populationCountry2, kilometersAreaCountry2);
+    float calculatePIBPerCapitaCountry1 = calculatePIBPerCapita(PIBCountry1, populationCountry1);
+    float calculatePIBPerCapitaCountry2 = calculatePIBPerCapita(PIBCountry2, populationCountry2);
+    float calculateSuperPowerCountry1 = calculateSuperPower(populationCountry1, kilometersAreaCountry1, turistsPointCountry1, PIBCountry1, densityCountry1);
+    float calculateSuperPowerCountry2 = calculateSuperPower(populationCountry2, kilometersAreaCountry2, turistsPointCountry2, PIBCountry2, densityCountry2);
 
+    printf("Escolha uma opção para comparar:\n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Número de pontos turísticos\n");
+    printf("5. Densidade demográfica\n");
+
+    // variavel para armazenar a escolha do jogador
+    int escolhaJogador;
+    scanf("%d", &escolhaJogador);
+
+    switch (escolhaJogador)
+    {
+    case 1:
+        // O nome dos dois países.
+        printf("Comparação entre: %s e %s\n", countryName1, countryName2);
+        // O atributo usado na comparação.
+        printf("O atributo usado na comparação foi: População\n");
+        // Os valores do atributo para cada carta.
+        printf("O valor de cada atributo é: (%d) para a carta 1,  e (%d) para a carta 2\n", populationCountry1, populationCountry2);
+
+        // Qual carta venceu.
+        if (populationCountry1 > populationCountry2)
+        {
+            printf("Resultado: Carta 1 (%s) venceu!", countryName1);
+        }
+        else if (populationCountry1 < populationCountry2)
+        {
+            printf("Resultado: Carta 2 (%s) venceu!", countryName2);
+        }
+        else
+        {
+            printf("Empate!");
+        }
+        break;
+
+    case 2:
+        // O nome dos dois países.
+        printf("Comparação entre: %s e %s\n", countryName1, countryName2);
+        // O atributo usado na comparação.
+        printf("O atributo usado na comparação foi: Área\n");
+        // Os valores do atributo para cada carta.
+        printf("O valor de cada atributo é: (%.2f) para a carta 1,  e (%.2f) para a carta 2\n", kilometersAreaCountry1, kilometersAreaCountry2);
+
+        // Qual carta venceu.
+        if (kilometersAreaCountry1 > kilometersAreaCountry2)
+        {
+            printf("Resultado: Carta 1 (%s) venceu!", countryName1);
+        }
+        else if (kilometersAreaCountry1 < kilometersAreaCountry2)
+        {
+            printf("Resultado: Carta 2 (%s) venceu!", countryName2);
+        }
+        else
+        {
+            printf("Empate!");
+        }
+        break;
+
+        case 3:
+            // O nome dos dois países.
+            printf("Comparação entre: %s e %s\n", countryName1, countryName2);
+            // O atributo usado na comparação.
+            printf("O atributo usado na comparação foi: PIB\n");
+            // Os valores do atributo para cada carta.
+            printf("O valor de cada atributo é: (%lf) para a carta 1,  e (%lf) para a carta 2\n", PIBCountry1, PIBCountry2);
     
-    // retornando a comparação entre duas cartas
-    printf("Comparação de cartas (Atributo: População):\n");
-    printf("Carta 1 - %s: %i\n", cityName1, populationCity1);
-    printf("Carta 2 - %s: %i\n", cityName2, populationCity2);
-    compairCityProperty(&populationCity1, &populationCity2, cityName1, cityName2);
+            // Qual carta venceu.
+            if (PIBCountry1 > PIBCountry2)
+            {
+                printf("Resultado: Carta 1 (%s) venceu!", countryName1);
+            }
+            else if (PIBCountry1 < PIBCountry2)
+            {
+                printf("Resultado: Carta 2 (%s) venceu!", countryName2);
+            }
+            else
+            {
+                printf("Empate!");
+            }
+            break;
+
+            case 4:
+                // O nome dos dois países.
+                printf("Comparação entre: %s e %s\n", countryName1, countryName2);
+                // O atributo usado na comparação.
+                printf("O atributo usado na comparação foi: Número de pontos turísticos\n");
+                // Os valores do atributo para cada carta.
+                printf("O valor de cada atributo é: (%d) para a carta 1,  e (%d) para a carta 2\n", PIBCountry1, PIBCountry2);
+        
+                // Qual carta venceu.
+                if (PIBCountry1 > PIBCountry2)
+                {
+                    printf("Resultado: Carta 1 (%s) venceu!", countryName1);
+                }
+                else if (PIBCountry1 < PIBCountry2)
+                {
+                    printf("Resultado: Carta 2 (%s) venceu!", countryName2);
+                }
+                else
+                {
+                    printf("Empate!");
+                }
+                break;
+
+       default:
+        break;
+    }
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,41 +1,3 @@
-# Desafio Super Trunfo - Países - Cadastro das Cartas - Atualizado: 21/02
-
-Bem-vindo ao desafio "Super Trunfo - Países"! No jogo Super Trunfo, os jogadores comparam os atributos das cartas para determinar a mais forte. O tema deste Super Trunfo é "Países", onde você comparará os atributos das cidades.
-
-A empresa MateCheck contratou você para desenvolver a parte inicial do jogo, que consiste no cadastro das cartas.
-
-O desafio está dividido em três níveis: Novato, Aventureiro e Mestre, com cada nível adicionando mais complexidade ao anterior.  **Você deve escolher qual desafio quer realizar.**
-
-🚨 **Atenção:** O nível Novato do desafio é focado apenas no cadastro das cartas, utilizando as funções `scanf` para ler os dados e `printf` para exibi-los.
-
-## 🎮 Nível Novato: Cadastro Básico
-
-No nível Novato, você iniciará criando o sistema básico do jogo Super Trunfo com o tema "Países". As cartas serão divididas por estados, cada um com quatro cidades.  Imagine um país dividido em oito estados (A a H), e cada estado com quatro cidades (1 a 4).  A combinação forma o código da carta (ex: A01, B02).
-
-🚩 **Objetivo:** Criar um programa em C que cadastra **duas** cartas com os seguintes atributos:
-
-*   População (`int`)
-*   Área (`float`)
-*   PIB (`float`)
-*   Número de pontos turísticos (`int`)
-
-⚙️ **Funcionalidades do Sistema:**
-
-*   O sistema permitirá ao usuário cadastrar os dados de **duas** cartas manualmente via terminal.
-*   Após o cadastro, o sistema exibirá os dados de cada cidade de forma organizada.
-
-📥 **Entrada** e 📤 **Saída de Dados:**
-
-*   O usuário insere os dados de cada carta interativamente via `scanf`.
-*   O programa exibe os dados cadastrados usando `printf`, com cada atributo em uma nova linha.
-
-**Simplificações para o Nível Novato:**
-
-*   Cadastre apenas **duas** cartas.
-*   Concentre-se na leitura, armazenamento e exibição. Não implemente comparações ou outros recursos.
-*   **Não use** laços (`for`, `while`) ou condicionais (`if`, `else`).
-
-
 ## 🛡️ Nível Aventureiro: Cálculo de Atributos
 
 No nível Aventureiro, você expandirá o sistema para incluir o cálculo de dois novos atributos: Densidade Populacional e PIB per Capita.
@@ -62,33 +24,62 @@ No nível Aventureiro, você expandirá o sistema para incluir o cálculo de doi
 *   Continue **sem usar** laços (`for`, `while`) ou condicionais (`if`, `else`).
 
 
+# Sobre o Projeto
 
-## 🏆 Nível Mestre: Comparação e Super Poder
+Este programa em C permite comparar atributos de duas cidades com base em dados fornecidos pelo usuário. O usuário insere informações sobre as cidades, como população, área, PIB e número de pontos turísticos. Em seguida, pode escolher um critério para comparar e determinar qual cidade tem a melhor pontuação nesse atributo.
 
-No nível Mestre, você implementará a comparação entre duas cartas e o cálculo do "Super Poder".
+## Pré-requisitos
 
-🆕 **Diferença em relação ao Nível Aventureiro:**
+Para compilar e executar este código, você precisa de um compilador C, como:
 
-*   **Comparação de Cartas:** O usuário poderá comparar as duas cartas.
-*   **Super Poder:** Soma de todos os atributos (inclusive os calculados), com a densidade populacional *invertida* antes da soma (1/densidade).  Tipo: `float`.
+- GCC (GNU Compiler Collection)
 
-⚙️ **Funcionalidades do Sistema:**
+- Clang
 
-*   Comparação atributo a atributo, mostrando qual carta venceu (1 se a Carta 1 vence, 0 se a Carta 2 vence).
-*   Para Densidade Populacional, vence a carta com o *menor* valor.
-*   Para os demais atributos (e o Super Poder), vence a carta com o *maior* valor.
+- MinGW (para Windows)
 
-📥 **Entrada** e 📤 **Saída de Dados:**
+## Como Compilar e Executar
 
-*   Mesma entrada dos níveis anteriores, mas a População agora é `unsigned long int`.
-*   A saída mostrará o resultado da comparação para cada atributo e o Super Poder.
+Passo 1: Compilar o código
 
-**Observação:**  Preste atenção à conversão de tipos ao calcular o Super Poder!
+1. Tenha a extensão `C/C++` instalada no seu vscode
+2. A extensão ira te gerar um botão na parte superior direita
+3. Ao clicar, vai se abrir o terminal do vscode.
 
 
-## 🏁 Conclusão
+## Como Testar
 
-Ao concluir qualquer um dos níveis, você terá dado um passo importante no desenvolvimento do Super Trunfo - Países. Boa sorte e divirta-se programando!
+1. O programa solicitará que você insira informações sobre duas cidades.
 
-Equipe de Ensino - MateCheck
-content_copy
+```
+Insira os seguintes dados para cada cidade:
+Letra do estado (A-H)
+
+Código da carta (exemplo: A01)
+
+Nome da cidade
+
+População
+
+Área em km²
+
+PIB da cidade
+
+Número de pontos turísticos
+```
+2. Após inserir os dados, escolha um critério para comparar as cidades:
+
+```
+1: População
+
+2: Área
+
+3: PIB
+
+4: Número de pontos turísticos
+
+5: Densidade demográfica
+```
+
+3. O programa mostrará os dados inseridos e indicará qual cidade tem o melhor valor para o critério escolhido.
+
