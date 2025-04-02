37f0ce60c3352b1296086d8235d6488e4cd40d2c
Vaniclevio Bernardo da silva <bernardooxente@gmail.com>
2025-04-02 18:18:28 +0000

    Fim de Teste ( Cartas )

--- Desenvolver Cartas Super Trunfo/xadrez.c
+++ Desenvolver Cartas Super Trunfo/xadrez.c
@@ -2,11 +2,57 @@
 
 int main(){
 
-    char carta [01];
-    char estado [10];
-    char codigo [5];
-    char cidade [20];
-    int população;
+    char carta[1];
+    char estado[10];
+    char codigo[5];
+    char cidade[20];
+    int populacao;
+    int pontosturisticos;
+    float area;
+    float pib;
+
+    
+    printf("Carta: \n");
+    scanf("%c", &carta);
+
+    printf("Digite seu estado: \n");
+    scanf("%s", &estado);
+
+    printf("codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("Nome da Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Total de Habitantes: \n");
+    scanf("%d", &populacao);
+
+    printf("Area: \n");
+    scanf("%f", &area);
+
+    printf("Pib: \n");
+    scanf("%f", &pib);
+
+    printf("Pontos Turisticos: %d \n", pontosturisticos);
+    scanf("%f", &area);
+    
+    printf("carta \n");
+    printf("Estado: %s \n", estado);
+    printf("Codigo da carta: %s%s \n", estado, codigo);
+    printf("Cidade: %s \n", cidade);
+    printf("Habitantes: %d \n", populacao);
+    printf("Area: %f \n",area);
+    printf("Pib: %n", pib);
+    printf("Pontos Turisticos: \n", pontosturisticos);
+
+    return 0;
+
+
+    char carta[2];
+    char estado[11];
+    char codigo[4];
+    char cidade[15];
+    int populacao;
     int pontosturisticos;
     float area;
     float pib;
@@ -24,26 +70,26 @@ int main(){
     scanf("%s", &cidade);
 
     printf("Total de Habitantes: \n");
-    scanf("%d", &população);
+    scanf("%d", &populacao);
 
     printf("Area: \n");
-    scanf("%d", &area);
+    scanf("%f", &area);
 
     printf("Pib: \n");
     scanf("%f", &pib);
 
-    printf("Pontos Turisticos: \n");
-    scanf("%d", &pontosturisticos);
-
+    printf("Pontos Turisticos: %d \n", pontosturisticos);
+    scanf("%f", &area);
     
     printf("carta \n");
     printf("Estado: %s \n", estado);
     printf("Codigo da carta: %s%s \n", estado, codigo);
     printf("Cidade: %s \n", cidade);
-    printf("Habitantes: %d \n", população);
+    printf("Habitantes: %d \n", populacao);
     printf("Area: %f \n",area);
     printf("Pib: %n", pib);
     printf("Pontos Turisticos: \n", pontosturisticos);
+
 
 
     return 0;
