5022cb1ff690cbf55bf9219de804f299a7adb527
c-aiorodrigo <caiorodrgo@gmail.com>
2025-04-06 22:32:42 -0300

    Update CartasSuperTrunfo.c
    com o super poder e comparação entre cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,7 @@ float pib1, pib2;
 int pturistico1, pturistico2;
 float densidade1, densidade2;
 float pibpc1, pibpc2;
+float SuperPoder1, SuperPoder2;
 
 //Entrada de dados para Carta 1:
 printf("Digite as informações sobre a Carta 1\n");
@@ -68,6 +69,10 @@ densidade2 = (float) populacao2 / areakm2;
 pibpc1 = (float) pib1 / populacao1;
 pibpc2 = (float) pib2 / populacao2;
 
+//Calculando o Super Poder
+SuperPoder1 = populacao1 + areakm1 + pib1 + pturistico1 + pibpc1 + (1 / densidade1); // 1/densidade pois isso inverte o valor, pois quando maior for o numero da densidade menor será o resultado desse calculo.
+SuperPoder2 = populacao2 + areakm2 + pib2 + pturistico2 + pibpc2 + (1 / densidade2); // seguindo a mesma logica.
+
 //Saída Carta 1:
 printf("\nCarta 1:\n");
 printf("Estado: %c\n", estado1);
@@ -79,6 +84,7 @@ printf("Pib: %.2f\n", pib1);
 printf("Pontos Turisticos: %d\n", pturistico1);
 printf("Densidade Demografica: %.2f\n", densidade1);
 printf("Pib Per Capita: %.2f\n", pibpc1);
+printf("Super Poder: %.3f\n", SuperPoder1);
 
 //Saída Carta 2: 
 printf("\nCarta 2:\n");
@@ -91,5 +97,16 @@ printf("Pib: %.2f\n", pib2);
 printf("Pontos Turisticos: %d\n", pturistico2);
 printf("Densidade Demografica: %.2f\n", densidade2);
 printf("Pib Per Capita: %.2f\n", pibpc2);
+printf("Super Poder: %.3f\n", SuperPoder2);
+
+//Comparação da Carta 1 em relação a Carta 2 (Carta 1 > Carta 2):
+printf("\n\nComparação: Carta 1 > Carta 2\n\n");
+printf("População: %d\n", (populacao1 > populacao2));
+printf("Area: %d\n", (areakm1 > areakm2));
+printf("Pib: %d\n", (pib1 > pib2));
+printf("Pontos Turisticos: %d\n", (pturistico1 > pturistico2));
+printf("Densidade: %d\n", (densidade1 > densidade2));
+printf("Pib per Capita: %d\n", (pibpc1 > pibpc2));
+printf("Super Poder: %d\n", (SuperPoder1 > SuperPoder2));
 }
 
