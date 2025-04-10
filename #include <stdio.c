85f5692085bcc3622bde1ac77826c6e12ee19275
Lucas-coder-lab <lucas-henrique_06@outlook.com>
2025-04-10 00:17:35 +0000

    verdadeiro

---
+++ #include <stdio.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//gerenciamento de estoque....
+
+int main(){
+
+ int a, b;
+ 
+ a = 2;
+ b = 3; 
+ condicao = a < b;
+
+  // falso é 0, e o verdadeiro se torna o 1.
+
+ printf("Sera que o numero %d e maior que o numero %d \n:", a, b);
+ printf(" O resultado logico e: %d", condicao);
+  
+
+
+
+
+
+
+
+   return 0;
+}
