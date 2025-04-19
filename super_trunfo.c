1c3c9cba83b8a26c07038a196aa92b46729f06b2
Davi Santana <batatovisk0330@gmail.com>
2025-04-19 23:50:34 +0000

    atualzaçao das variaveis

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+
    int main()
-   {
-       int População, Pontos turisticos;
-       double Área, Pib;
-       char Estado ;
-       char nome[50];
-
-
-
-
+   {   
+       // Variaveis para carta 1
+       int população1, pontosTuristicos1;
+       float area1, pib1;
+       char estado1;
+       char nomeCidade1[30];
+       char codigo1[5];
+       
+       // Variaveis para carta 2
+       int população2, pontosTuristicos2;
+       float area2, pib2;
+       char estado2;
+       char nomeCidade2[30];
+       char codigo2[5];
 
 
    }
