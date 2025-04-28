ab40431fa29c8bfdfeb36d107ac5eb75df6d5baf
Kaio Greco <94496912+KaioGrecoOl@users.noreply.github.com>
2025-04-28 21:38:28 +0000

    Exibindo os dados das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -68,5 +68,24 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
+     // Exibição dos dados
+     printf("\nCarta 1:\n");
+     printf("Estado: %c\n", estado1);
+     printf("Código: %s\n", codigo1);
+     printf("Nome da Cidade: %s\n", nomeCidade1);
+     printf("População: %d\n", populacao1);
+     printf("Área: %.2f km²\n", area1);
+     printf("PIB: %.2f bilhões de reais\n", pib1);
+     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+ 
+     printf("\nCarta 2:\n");
+     printf("Estado: %c\n", estado2);
+     printf("Código: %s\n", codigo2);
+     printf("Nome da Cidade: %s\n", nomeCidade2);
+     printf("População: %d\n", populacao2);
+     printf("Área: %.2f km²\n", area2);
+     printf("PIB: %.2f bilhões de reais\n", pib2);
+     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+
     return 0;
 }
